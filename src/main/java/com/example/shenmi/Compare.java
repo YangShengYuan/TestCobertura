bddca9caba0ba987d1e3d86c24d75cc3f14e4a68
iridescent <1834699623@qq.com>
2019-05-22 01:27:33 +0800

    ss

---
+++ src/main/java/com/example/shenmi/Compare.java
@@ -0,0 +1,18 @@
+package com.example.shenmi;
+
+/**
+ * @author Yang ShengYuan
+ * @date 2019/5/22
+ * @Description ${DESCRIBE}
+ **/
+public class Compare {
+    public int compareInt(int x, int y){
+        if(x>y){
+            return 1;
+        }else if (x<y){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/com/example/shenmi/Compute.java
@@ -0,0 +1,18 @@
+package com.example.shenmi;
+
+/**
+ * @author Yang ShengYuan
+ * @date 2019/5/22
+ * @Description ${DESCRIBE}
+ **/
+public class Compute {
+    public int add(int x, int y){
+        return x+y;
+    }
+    public int mul(int x, int y){
+        return x*y;
+    }
+    public int minus(int x, int y){
+        return Math.abs(x-y);
+    }
+}
