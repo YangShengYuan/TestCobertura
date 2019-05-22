27376c335f38b0113b9bae5c05fdfe74e2cfddc0
iridescent <1834699623@qq.com>
2019-05-22 10:15:59 +0800

    sss

---
+++ src/test/java/com/example/shenmi/TestSuits.java
@@ -0,0 +1,14 @@
+package com.example.shenmi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Yang ShengYuan
+ * @date 2019/5/22
+ * @Description ${DESCRIBE}
+ **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ComputeTest.class,CompareTest.class})
+public class TestSuits {
+}
